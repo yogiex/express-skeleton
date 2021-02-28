646cb3215684ef281e19c139b546cf52472a86d6
yogiex <priyagung.yoggi@gmail.com>
2021-03-01 00:14:05 +0700

    malam

--- auth/jwt.strategy.js
+++ auth/jwt.strategy.js
@@ -2,21 +2,31 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 function generateAccessToken(username)  {
-    const token = jwt.sign(username,process.env.SECRET_KEY, {expiresIn:'2h',algorithm:'HS512'});
-    return token;
+    return jwt.sign(username,process.env.SECRET_KEY, {expiresIn:'2h'});
+    //return token;
 }
 function authenticateToken(req,res,next){
     const authHeader = req.headers['authorization'];
     //const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1]
-    if(token == null) return res.sendStatus(401)
+    //const authHeader = req.headers['token']
+    // const token = authHeader && authHeader.split(' ')[1]
+    // if(token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ,(err,user) =>{
-        //console.log(err)
-        if(err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
+    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err,user) =>{
+    //     //console.log(err)
+    //     if(err) return res.sendStatus(403)
+    //     req.user = user
+    //     next()
+    // })
+    if(typeof authHeader !== 'undefined'){
+        const bearer = authHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+    
 }
 module.exports =  generateAccessToken, authenticateToken;
                   
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userModel = require('./model/user');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const bodyparser = require('body-parser');
-const passport = require('passport')
+const jwt = require('jsonwebtoken');
 //auth service
 const generateAccessToken = require('./auth/jwt.strategy');
 const authenticateToken = require('./auth/jwt.strategy');
@@ -22,14 +22,24 @@ Mongoose.connect(process.env.MONGO_URI_TEST,{
 })
 app.use(morgan('dev'));
 
-
+app.use(function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next()
+})
 app.get('/ping', async (req,res) => {
    return await res.send({msg:'PONG!'})
 })
 
 app.get('/auth/testing', authenticateToken, (req,rest) => {
-
-    return res.json({msg:'auth works'})
+    // const users = new userModel()
+    //  res.json(users.filter(user => user.username === req.users.email))
+    //authenticateToken(req.token,process.env.ACCESS_TOKEN_SECRET)
+    //res.header("x-auth-token")
+    
+    
+    res.send({msg:"im reach heree guyssss"})
 })
 
 // app.get('/rahasia', passport.authenticate('jwt',{session:false}),(req,res,next) =>{
@@ -82,7 +92,8 @@ app.post('/auth/login', async(req,res) => {
             const cmp = await bcrypt.compare(req.body.password,user.password)
             
             if(cmp){
-                const token = generateAccessToken({user:req.body.email,role:req.body.role})
+                const token = generateAccessToken({email:req.body.email,role:user.role})
+                //const token = generateAccessToken({user})
                 res.status(200).json({
                     status:'auth works',
                     token: token
