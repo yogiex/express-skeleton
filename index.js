ecff181c9e9e6169a9b5f41e28f9c50123191207
yogiex <priyagung.yoggi@gmail.com>
2021-03-03 10:32:56 +0700

    git ignore

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const bodyparser = require('body-parser');
 const jwt = require('jsonwebtoken');
+
+const cookieParser = require('cookie-parser')
 //auth service
 //const auth = require('./auth/jwt.strategy');
 const {authenticateToken, generateAccessToken} = require('./auth/jwt.strategy');
@@ -15,6 +17,7 @@ const passport = require('passport')
 require('dotenv').config()
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser()); //cookieee
 //app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
 Mongoose.connect(process.env.MONGO_URI_TEST,{
@@ -24,44 +27,25 @@ Mongoose.connect(process.env.MONGO_URI_TEST,{
 })
 app.use(morgan('dev'));
 
-// app.use(function(req,res,next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//     next()
-// })
+app.use(function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next()
+})
+
 app.get('/ping', async (req,res) => {
    return await res.send({msg:'PONG!'})
 })
 
 app.get('/auth/testing', authenticateToken, (req,res) => {
-    // const users = new userModel()
-    //  res.json(users.filter(user => user.username === req.users.email))
-    //authenticateToken(req.token,process.env.ACCESS_TOKEN_SECRET)
-    //res.header("x-auth-token")
-    
-    
     res.send({msg:"im reach heree guyssss"})
 })
 
-// app.get('/rahasia', passport.authenticate('jwt',{session:false}),(req,res,next) =>{
-//  res.send('rahasia')
-// })
-// app.get('/token', async (req,res) =>{
-//     const user = await new userModel()
-//     const token = await generateAccessToken(user)
-//     res.send({token})
-// })
 app.post('/auth/register', async (req,res) => {
     // do something stuff
     const {email, username, password} = req.body;
     
-    //const hashp = await bcrypt.hash(req.body.password,10)
-    // const user = await new userModel({
-    //     email: req.body.email,
-    //     username: req.body.username,
-    //     password: req.body.password,
-    // })
 
     try {
         const token = generateAccessToken({username}); // generate token pas register
@@ -83,19 +67,7 @@ app.post('/auth/register', async (req,res) => {
     }
 
 })
-// function authenticateToken(req,res,next){
-//     //const authHeader = req.headers['authorization'];
-//     const authHeader = req.headers.authorization;
-//     const token = authHeader && authHeader.split(" ")[1]
-//     if(token == null) return res.sendStatus(401)
-   
-//     jwt.verify(token, process.env.SECRET_KEY , (err,user) =>{
-//         if(err) return res.sendStatus(403)
-//         req.user = user._id
-//         next()
-//     })
-    
-// }
+
 app.post('/auth/login', async(req,res) => {
     
     try {
@@ -111,7 +83,11 @@ app.post('/auth/login', async(req,res) => {
                     status:'auth works',
                     token: token
                 })
-                
+                // res.writeHead(200,{
+                //     "Set-Cookie" :token,
+                //     "Access-Control-Allow-Credentials": "true"
+                // })
+                // .send()
                 
                 //res.cookie("SESSIONID",token,{httpOnly:true,secure:true})
             }else{
